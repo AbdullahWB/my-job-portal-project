545d4d8c70d322a8882dd22a4a475cdda1eff1e9
AbdullahWB <abdullah917828@gmail.com>
2023-05-16 20:21:42 +0600

    make footer

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../../Shared/Navbar/Navbar';
 import Banner from '../../Shared/Banner/Banner';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Banner></Banner>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
